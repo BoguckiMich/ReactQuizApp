06f89324908a0309ed7148675aea19f5d43106c2
BoguckiMich <bogucki.mich@gmail.com>
2020-08-19 14:06:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const API_URL = 'https://opentdb.com/api.php?amount=10&category=31'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="container">
+    <div className="bg-yellow-200  text-blue-500 p-8 rounded-lg shadow-lg">
+      <h2 className="text-2xl">
+      question here which will a bit longer than just question here
+      </h2>
     </div>
-  );
+    <div className="flex flex-wrap justify-evenly mt-4">
+      <button className="mt-1 bg-yellow-200 w-2/5 p-4 text-blue-500 font-semibold rounded-lg shadow-md">Answer1</button>
+      <button className="mt-1 bg-yellow-200 w-2/5 p-4 text-blue-500 font-semibold rounded-lg shadow-md">Answer2</button>
+      <button className="mt-1 bg-yellow-200 w-2/5 p-4 text-blue-500 font-semibold rounded-lg shadow-md">Answer3</button>
+      <button className="mt-1 bg-yellow-200 w-2/5 p-4 text-blue-500 font-semibold rounded-lg shadow-md">Answer4</button>
+    </div>
+  </div>)
 }
 
 export default App;
