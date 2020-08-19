e338dc1fcac8be8853a13dd05729ebb83e462041
BoguckiMich <bogucki.mich@gmail.com>
2020-08-19 15:00:00 +0200

    changed from flex to grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 const API_URL = 'https://opentdb.com/api.php?amount=10&category=31'
+const answerStyle = " bg-yellow-200  p-4 text-blue-500 font-semibold rounded-lg shadow-md"
 
 function App() {
+
+  const [questions, setQuestion] = useState([])
+
+  useEffect(()=>{
+
+  }, [])
   return (
   <div className="container">
     <div className="bg-yellow-200  text-blue-500 p-8 rounded-lg shadow-lg">
@@ -11,11 +18,11 @@ function App() {
       question here which will a bit longer than just question here
       </h2>
     </div>
-    <div className="flex flex-wrap justify-evenly mt-4">
-      <button className="mt-1 bg-yellow-200 w-2/5 p-4 text-blue-500 font-semibold rounded-lg shadow-md">Answer1</button>
-      <button className="mt-1 bg-yellow-200 w-2/5 p-4 text-blue-500 font-semibold rounded-lg shadow-md">Answer2</button>
-      <button className="mt-1 bg-yellow-200 w-2/5 p-4 text-blue-500 font-semibold rounded-lg shadow-md">Answer3</button>
-      <button className="mt-1 bg-yellow-200 w-2/5 p-4 text-blue-500 font-semibold rounded-lg shadow-md">Answer4</button>
+    <div className="grid grid-cols-2 gap-6 mt-6">
+      <button className={answerStyle}>Answer1</button>
+      <button className={answerStyle}>Answer2</button>
+      <button className={answerStyle}>Answer3</button>
+      <button className={answerStyle}>Answer4</button>
     </div>
   </div>)
 }
