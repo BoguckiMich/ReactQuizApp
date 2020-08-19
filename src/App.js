edb5691dddf0de3f5a055cbfd1b510fe2995932d
BoguckiMich <bogucki.mich@gmail.com>
2020-08-19 19:32:32 +0200

    questionaire component created, randomize of answers, right answer selected

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import Questionaire from "./components/Questionaire"
 
-const API_URL = "https://opentdb.com/api.php?amount=10&category=31";
-const answerStyle =
-  " bg-yellow-200  p-4 text-blue-500 font-semibold rounded-lg shadow-md";
+const API_URL = "https://opentdb.com/api.php?amount=10&category=31&type=multiple";
+
 
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState()
 
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
         setQuestions(data.results);
+        setCurrentQuestion(data.results[0])
       });
   }, []);
+
+  const handleAnswer = (answer) => {
+    //todo
+  }
+
   return questions.length > 0 ? (
     <div className="container">
-      <div className="bg-yellow-200  text-blue-500 p-8 rounded-lg shadow-lg">
-        <h2 className="text-2xl">{questions[0].question}</h2>
-      </div>
-      <div className="grid grid-cols-2 gap-6 mt-6">
-        <button className={answerStyle}>{questions[0].correct_answer}</button>
-        <button className={answerStyle}>
-          {questions[0].incorrect_answers[0]}
-        </button>
-        <button className={answerStyle}>
-          {questions[0].incorrect_answers[1]}
-        </button>
-        <button className={answerStyle}>
-          {questions[0].incorrect_answers[2]}
-        </button>
-      </div>
+      {currentQuestion && (
+        <Questionaire data={currentQuestion} handleAnswer={handleAnswer} className />
+      )}
     </div>
   ) : (
-    <h1>Question is loading! Wait a sec !</h1>
-  );
+      <h1>Question is loading! Wait a sec !</h1>
+    );
 }
 
 export default App;
---
+++ src/components/Questionaire.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+// const answerStyle =
+//   " bg-yellow-200  p-4 text-blue-500 font-semibold rounded-lg shadow-md";
+
+// const Button = ({ answer, className }) => (
+//   <button className={`bg-yellow-200  p-4 text-blue-500 font-semibold rounded-lg shadow-md ${className}`}>{answer}</button>
+// )
+
+
+const Questionaire = ({ handleAnswer, data: { question, correct_answer, incorrect_answers } }) => {
+  const shuffledAnswers = [correct_answer, ...incorrect_answers].sort(() => Math.random() - 0.5)
+
+  return (
+    <div>
+      <div className="bg-yellow-200  text-blue-500 p-8 rounded-lg shadow-lg">
+        <h2
+          className="text-2xl"
+          dangerouslySetInnerHTML={{ __html: question }}
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-6 mt-6">
+        {shuffledAnswers.map((answer) => (
+          <button className={`${correct_answer === answer ? 'bg-blue-200' : 'bg-yellow-200'} p-4 text-blue-500 font-semibold rounded-lg shadow-md`} onClick={() => handleAnswer(answer)}>{answer}</button>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Questionaire;
