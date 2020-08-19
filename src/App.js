c85e55d9bafc3a14c7f6166be08bfdf14b35f808
BoguckiMich <bogucki.mich@gmail.com>
2020-08-19 17:40:08 +0200

    fetch for questions is working

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-const API_URL = 'https://opentdb.com/api.php?amount=10&category=31'
-const answerStyle = " bg-yellow-200  p-4 text-blue-500 font-semibold rounded-lg shadow-md"
+const API_URL = "https://opentdb.com/api.php?amount=10&category=31";
+const answerStyle =
+  " bg-yellow-200  p-4 text-blue-500 font-semibold rounded-lg shadow-md";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
 
-  const [questions, setQuestion] = useState([])
-
-  useEffect(()=>{
-
-  }, [])
-  return (
-  <div className="container">
-    <div className="bg-yellow-200  text-blue-500 p-8 rounded-lg shadow-lg">
-      <h2 className="text-2xl">
-      question here which will a bit longer than just question here
-      </h2>
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestions(data.results);
+      });
+  }, []);
+  return questions.length > 0 ? (
+    <div className="container">
+      <div className="bg-yellow-200  text-blue-500 p-8 rounded-lg shadow-lg">
+        <h2 className="text-2xl">{questions[0].question}</h2>
+      </div>
+      <div className="grid grid-cols-2 gap-6 mt-6">
+        <button className={answerStyle}>{questions[0].correct_answer}</button>
+        <button className={answerStyle}>
+          {questions[0].incorrect_answers[0]}
+        </button>
+        <button className={answerStyle}>
+          {questions[0].incorrect_answers[1]}
+        </button>
+        <button className={answerStyle}>
+          {questions[0].incorrect_answers[2]}
+        </button>
+      </div>
     </div>
-    <div className="grid grid-cols-2 gap-6 mt-6">
-      <button className={answerStyle}>Answer1</button>
-      <button className={answerStyle}>Answer2</button>
-      <button className={answerStyle}>Answer3</button>
-      <button className={answerStyle}>Answer4</button>
-    </div>
-  </div>)
+  ) : (
+    <h1>Question is loading! Wait a sec !</h1>
+  );
 }
 
 export default App;
